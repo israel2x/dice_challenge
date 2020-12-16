01e6a8e58ed5b92cb5ce877e2ab1baa7c267b480
Israel Zurita <israel.5k@hotmail.com>
2020-12-15 19:49:03 -0500

    change colors and cursor

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -11,12 +11,14 @@ const Dice = ({ diceImg, diceNum, setRandomNum }) => {
             "box-shadow":
               " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
             "border-radius": "24px",
+            cursor: "pointer",
+            transition: "0.2s all",
           }}
         />
       </div>
       <div>
         <p style={{ textAlign: "center" }}>
-          <strong>{diceNum}</strong>
+          <strong style={{ color: "#3399ff" }}>{diceNum}</strong>
         </p>
       </div>
     </div>
